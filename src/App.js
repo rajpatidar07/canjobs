5c81b46857873d0e8db36b2443b295d5517d850d
rajpatidar07 <101445589+rajpatidar07@users.noreply.github.com>
2023-02-25 11:08:33 +0530

    warning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // import logo from './logo.svg';
 import React from "react";
 import "./App.css";
-import EmployerLayout from "./components/employer/layout";
 import EmployeeLayout from "./components/employee/layout";
 
 function App() {
   return (
     <div className="App">
       <EmployeeLayout />
-      {/* <EmployerLayout /> */}
     </div>
   );
 }
