4716539db2258e7974750850436a81db5702d9dd
Aashi vyas <aashi.we2code@gmail.com>
2023-02-28 18:33:16 +0530

    set validation in education form.

--- src/components/forms/education.js
+++ src/components/forms/education.js
@@ -1,7 +1,64 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
+import useValidation from "../common/useValidation";
 
 function Education(props) {
+  /*----USER Education VALIDATION----*/
+  const initialFormState = {
+    qualification: "",
+    university: "",
+    course: "",
+    specialization: "",
+    location: "",
+    passingYear: "",
+  };
+  /*----VALIDATION CONTENT----*/
+  const validators = {
+    qualification: [
+      (value) =>
+        value === null || value.trim() === ""
+          ? "Qualification is required"
+          : null,
+    ],
+    university: [
+      (value) =>
+        value === null || value.trim() === "" ? "University is required" : null,
+    ],
+    course: [
+      (value) =>
+        value === null || value.trim() === "" ? "Course is required" : null,
+    ],
+    specialization: [
+      (value) =>
+        value === null || value.trim() === ""
+          ? "Specialization is required"
+          : null,
+    ],
+    location: [
+      (value) =>
+        value === null || value.trim() === "" ? "Location is required" : null,
+    ],
+    passingYear: [
+      (value) =>
+        value === "" || value === null ? "Passing Year is required" : null,
+    ],
+  };
+  /*----LOGIN ONCHANGE FUNCTION----*/
+  const { state, onInputChange, errors, validate } = useValidation(
+    initialFormState,
+    validators
+  );
+
+  /*----LOGIN SUBMIT FUNCTION----*/
+  const onEducationSubmitClick = (event) => {
+    event.preventDefault();
+
+    if (validate()) {
+      // handle form submission
+    }
+  };
+  // END USER Education VALIDATION
+
   return (
     <>
       <Modal
@@ -20,19 +77,51 @@ function Education(props) {
         </button>
         {/* <div className="modal-dialog max-width-px-540 position-relative"> */}
         <div className="bg-white rounded h-100 px-11 pt-7">
-          <form>
+          <form onSubmit={onEducationSubmitClick}>
             <h5 className="text-center pt-2">Education Details</h5>
             <div className="row pt-5">
               {" "}
               <div className="form-group col-md-6">
-                <label className="font-size-4 text-black-2 font-weight-semibold line-height-reset">
+                <label
+                  htmlFor="qaualification"
+                  className="font-size-4 text-black-2 font-weight-semibold line-height-reset"
+                >
                   Qualification <span className="text-danger">*</span> :
                 </label>
-                <select className="form-control" id="Qualification">
-                  <option value={""}>select Qualification</option>
-                  <option value={""}>Doctors</option>
-                  <option value={""}>Masters</option>
+                <select
+                  className={
+                    errors.qualification
+                      ? "form-control border border-danger"
+                      : "form-control"
+                  }
+                  name="qaualification"
+                  id="qaualification"
+                  // value={state.qualification}
+                  onChange={onInputChange}
+                >
+                  <option
+                    defaultValue={state.qualification}
+                    onChange={onInputChange}
+                  >
+                    select Qualification
+                  </option>
+                  <option value={"Doctors"} onChange={onInputChange}>
+                    Doctors
+                  </option>
+                  <option value={"Masters"} onChange={onInputChange}>
+                    Masters
+                  </option>
                 </select>
+                {/*----ERROR MESSAGE FOR QUALIFICATION----*/}
+                {errors.qualification && (
+                  <span>
+                    {errors.qualification.map((error) => (
+                      <span key={error} className="text-danger font-size-3">
+                        {error}
+                      </span>
+                    ))}
+                  </span>
+                )}
               </div>
               <div className="form-group col-md-6">
                 <label
@@ -124,7 +213,10 @@ function Education(props) {
               </div>
             </div>
             <div className="form-group text-center">
-              <button className="btn btn-primary btn-small w-25 rounded-5 text-uppercase">
+              <button
+                className="btn btn-primary btn-small w-25 rounded-5 text-uppercase"
+                type="submit"
+              >
                 Submit
               </button>
             </div>
