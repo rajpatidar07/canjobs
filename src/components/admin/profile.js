96fe397d0eae4fe16fa6bf1e6056af813b7c08d2
Aashi vyas <aashi.we2code@gmail.com>
2023-03-02 18:34:56 +0530

    Set validation for updating the admin profile .

--- src/components/admin/profile.js
+++ src/components/admin/profile.js
@@ -1,8 +1,70 @@
 import React from "react";
 import AdminHeader from "./header";
 import AdminSidebar from "./sidebar";
+import useValidation from "../common/useValidation";
 
 function Profile() {
+  // USER ADMIN PROFILE UPDATE VALIDATION
+
+  // INITIAL STATE ASSIGNMENT
+  const initialFormState = {
+    adminname: "",
+    adminemail: "",
+    adminpassword: "",
+    location: "",
+    admintype: "",
+  };
+  // VALIDATION CONDITIONS
+  const validators = {
+    adminname: [
+      (value) =>
+        value === "" || value.trim() === "" ? "Admin type is required" : null,
+    ],
+    adminemail: [
+      (value) =>
+        value === "" || value.trim() === ""
+          ? "Email is required"
+          : /\S+@\S+\.\S+/.test(value)
+          ? null
+          : "Email is invalid",
+    ],
+    adminpassword: [
+      (value) =>
+        value === ""
+          ? "Password is required"
+          : /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(
+              value
+            )
+          ? null
+          : "Password must contain digit, one uppercase letter, one special character, no space, and it must be 8-16 characters long",
+    ],
+    location: [
+      (value) =>
+        value === "" || value.trim() === ""
+          ? "location is required"
+          : /[^A-Za-z 0-9]/g.test(value)
+          ? "Cannot use special character "
+          : null,
+    ],
+    admintype: [
+      (value) =>
+        value === "" || value.trim() === "" ? "Admin type is required" : null,
+    ],
+  };
+  // CUSTOM VALIDATIONS IMPORT
+  const { state, onInputChange, errors, validate } = useValidation(
+    initialFormState,
+    validators
+  );
+
+  // USER ADMIN PROFILE UPDATE SUBMIT BUTTON
+  const onAminProfileUpdateClick = (event) => {
+    event.preventDefault();
+    if (validate()) {
+    }
+  };
+  // END USER ADMIN PROFILE UPDATE VALIDATION
+
   return (
     <>
       <div className="site-wrapper overflow-hidden bg-default-2">
@@ -10,16 +72,6 @@ function Profile() {
         <AdminHeader />
         {/* <!-- navbar- --> */}
         <AdminSidebar />
-        {/* <a
-          className="sidebar-mobile-button"
-          data-toggle="collapse"
-          href="#sidebar"
-          role="button"
-          aria-expanded="false"
-          aria-controls="sidebar"
-        >
-          <i className="icon icon-sidebar-2"></i>
-        </a> */}
         <div className="dashboard-main-container mt-24" id="dashboard-body">
           <div className="container">
             <div className="mb-15 mb-lg-23">
@@ -33,29 +85,44 @@ function Profile() {
                       {/* <!-- <img id="imgPrime" src="" alt="uploaded image placeholder" /> --> */}
                       {/* <!-- <input type="file" id="upfile"> --> */}
                     </div>
-                    <form action="/">
+                    <form onSubmit={onAminProfileUpdateClick}>
                       <fieldset>
                         <div className="row mb-xl-1 mb-9">
                           <div className="col-lg-6">
                             <div className="form-group">
                               <label
-                                htmlFor="namedash"
+                                htmlFor="adminname"
                                 className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
                               >
                                 Name
                               </label>
                               <input
                                 type="text"
-                                className="form-control h-px-48"
-                                id="namedash"
+                                className={
+                                  errors.adminname
+                                    ? "form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4 border border-danger"
+                                    : "form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4"
+                                }
+                                value={state.adminname}
+                                onChange={onInputChange}
+                                id="adminname"
+                                name="adminname"
                                 placeholder="eg. Apple"
                               />
+                              {errors.adminname && (
+                                <span
+                                  key={errors.adminname}
+                                  className="text-danger font-size-3"
+                                >
+                                  {errors.adminname}
+                                </span>
+                              )}
                             </div>
                           </div>
                           <div className="col-lg-6">
                             <div className="form-group">
                               <label
-                                htmlFor="select2"
+                                htmlFor="adminemail"
                                 className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
                               >
                                 E-mail
@@ -63,27 +130,62 @@ function Profile() {
                               <input
                                 type="email"
                                 placeholder="Email12@gmail.com"
-                                className="form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4"
+                                className={
+                                  errors.adminemail
+                                    ? "form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4 border border-danger"
+                                    : "form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4"
+                                }
+                                value={state.adminemail}
+                                onChange={onInputChange}
+                                id="adminemail"
+                                name="adminemail"
                               />
+                              {errors.adminemail && (
+                                <span
+                                  key={errors.adminemail}
+                                  className="text-danger font-size-3"
+                                >
+                                  {errors.adminemail}
+                                </span>
+                              )}
                             </div>
                           </div>
                         </div>
                         <div className="row mb-8">
                           <div className="col-lg-6 mb-xl-0 mb-7">
                             <div className="form-group position-relative">
-                              <label className="d-block text-black-2 font-size-4 font-weight-semibold mb-4">
+                              <label
+                                htmlFor="adminpassword"
+                                className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
+                              >
                                 Password
                               </label>
                               <input
                                 type="password"
-                                className="form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4"
+                                className={
+                                  errors.adminpassword
+                                    ? "form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4 border border-danger"
+                                    : "form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4"
+                                }
+                                value={state.adminpassword}
+                                onChange={onInputChange}
+                                id="adminpassword"
+                                name="adminpassword"
                               />
+                              {errors.adminpassword && (
+                                <span
+                                  key={errors.adminpassword}
+                                  className="text-danger font-size-3"
+                                >
+                                  {errors.adminpassword}
+                                </span>
+                              )}
                             </div>
                           </div>
                           <div className="col-lg-6">
                             <div className="form-group position-relative">
                               <label
-                                htmlFor="address"
+                                htmlFor="location"
                                 className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
                               >
                                 Location
@@ -91,31 +193,77 @@ function Profile() {
                               <input
                                 type="text"
                                 placeholder="Premises"
-                                className="form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4"
+                                className={
+                                  errors.location
+                                    ? "form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4 border border-danger"
+                                    : "form-control nice-select pl-6 arrow-3 h-px-48 w-100 font-size-4"
+                                }
+                                value={state.location}
+                                onChange={onInputChange}
+                                id="location"
+                                name="location"
                               />
-                              <span className="h-100 w-px-50 pos-abs-tl d-flex align-items-center justify-content-center font-size-6"></span>
+                              {errors.location && (
+                                <span
+                                  key={errors.location}
+                                  className="text-danger font-size-3"
+                                >
+                                  {errors.location}
+                                </span>
+                              )}
                             </div>
                           </div>
                         </div>
                         <div className="row">
                           <div className="col-md-12">
                             <div className="form-group">
-                              <label className="d-block text-black-2 font-size-4 font-weight-semibold mb-4">
+                              <label
+                                htmlFor="admintype"
+                                className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
+                              >
                                 Admin Type
                               </label>
                               <div className="position-relative">
-                                <select className="form-control col-6">
+                                <select
+                                  className={
+                                    errors.admintype
+                                      ? "form-control col-6 border border-danger"
+                                      : "form-control col-6"
+                                  }
+                                  value={state.admintype}
+                                  onChange={onInputChange}
+                                  name="admintype"
+                                  id="admintype"
+                                >
                                   <option value={""}>select type</option>
-                                  <option value={""}>Manager</option>
-                                  <option value={""}>Sub admin</option>
-                                  <option value={""}>Admin</option>
-                                  <option value={""}>Super admin</option>
+                                  <option value={"manager"}>Manager</option>
+                                  <option value={"subadmin"}>Sub admin</option>
+                                  <option value={"admin"}>Admin</option>
+                                  <option value={"superadmin"}>
+                                    Super admin
+                                  </option>
                                 </select>
+                                {errors.admintype && (
+                                  <span
+                                    key={errors.admintype}
+                                    className="text-danger font-size-3"
+                                  >
+                                    {errors.admintype}
+                                  </span>
+                                )}
                               </div>
                             </div>
                           </div>
                         </div>
-                      </fieldset>
+                        <div className="form-group text-center">
+                          <button
+                            className="btn btn-primary btn-small w-25 rounded-5 text-uppercase"
+                            type="submit"
+                          >
+                            Update
+                          </button>
+                        </div>
+                      </fieldset>{" "}
                     </form>
                   </div>
                 </div>
