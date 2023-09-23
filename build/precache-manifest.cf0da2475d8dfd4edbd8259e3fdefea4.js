8c1c3ee1d18fffd4bc56d49195e301929f6df87a
rajpatidar07 <101445589+rajpatidar07@users.noreply.github.com>
2023-09-23 14:24:00 +0530

    sdsd

---
+++ build/precache-manifest.cf0da2475d8dfd4edbd8259e3fdefea4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "19bfeeb198d28ff41ee3",
+    "url": "/static/css/main.931f0a42.chunk.css"
+  },
+  {
+    "revision": "19bfeeb198d28ff41ee3",
+    "url": "/static/js/main.800c46f0.chunk.js"
+  },
+  {
+    "revision": "e36821d9a2173fbc7c9a",
+    "url": "/static/js/runtime~main.b56548d0.js"
+  },
+  {
+    "revision": "6f1acb17175132b109c8",
+    "url": "/static/css/2.49febab2.chunk.css"
+  },
+  {
+    "revision": "6f1acb17175132b109c8",
+    "url": "/static/js/2.90d84d50.chunk.js"
+  },
+  {
+    "revision": "10fc9320c2e57ac3ca4d",
+    "url": "/static/js/3.39587857.chunk.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "ce38b86ea5173a6e60dc02a219a47094",
+    "url": "/static/media/verified.ce38b86e.png"
+  },
+  {
+    "revision": "79393c9a8db5319373f31fb01bf61128",
+    "url": "/index.html"
+  }
+];
