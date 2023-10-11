ecf2b6696e83fbe49f60847ad876e8375562ff2e
Aashi vyas <141002722+Aashi.we2code@users.noreply.github.com>
2023-10-11 09:37:05 +0530

    Changed the backend url.

--- .history/src/api/api_20231010183131.js
+++ .history/src/api/api_20231010183131.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 // const API_URL = "https://192.168.29.92/canjobs/";
 // const API_URL = "http://65.0.20.158/canjobs/";
-const API_URL = "https://apnaorganicstore.in/canjobs/";
-// const API_URL = "http://51.20.6.80/canjobs/";
+// const API_URL = "https://apnaorganicstore.in/canjobs/";
+const API_URL = "http://51.20.6.80/canjobs/";
 // const API_URL = "http://192.168.29.92/canjobs_october/";
 let Token = localStorage.getItem("token");
 const view_as_token = localStorage.getItem("view_as_token");
