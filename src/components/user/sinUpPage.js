f9a74b9f8c76bb0e55784aa0924aa2736dadd67e
rajpatidar07 <101445589+rajpatidar07@users.noreply.github.com>
2023-10-20 21:28:11 +0530

    css import

--- src/components/user/sinUpPage.js
+++ src/components/user/sinUpPage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import UserRegisterPage from "../common/userRegisterPage";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../App.css";
 function SinUpPage() {
   return (
     <div>
-      <link
+      {/* <link
         rel="stylesheet"
         href="https://canjobs.vercel.app/css/bootstrap.css"
       />
-      <link rel="stylesheet" href="https://canjobs.vercel.app/css/main.css" />
+      <link rel="stylesheet" href="https://canjobs.vercel.app/css/main.css" /> */}
       <UserRegisterPage />
     </div>
   );
