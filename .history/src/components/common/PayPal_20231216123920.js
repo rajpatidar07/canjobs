42ce7d61bcc5fbbc2d4c6248ffec8c74947a4312
Aashi vyas <141002722+Aashi.we2code@users.noreply.github.com>
2023-12-16 17:17:03 +0530

    Latest code.

---
+++ .history/src/components/common/PayPal_20231216123920.js
@@ -0,0 +1,123 @@
+import React from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { toast } from "react-toastify";
+import { AddPayPalpay } from "../../api/api";
+// import { getAccessToken } from "./paypalAuthTokeGet.js";
+// import { getTransactionDetails } from "./paymentDetail.js";
+
+const PayPalButton = (props) => {
+  let user_id = localStorage.getItem("employee_id");
+  // const [payAmt, setPayAmt] = useState(props.amount);
+  console.log("run-----onchangeInput-----" + props.amount);
+  const createOrder = async (data, actions) => {
+    let amt = await props.getAmt();
+    console.log("chk---amt" + amt);
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: amt, // Example amount (update with your actual amount)
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    // Capture the funds from the transaction
+    return actions.order.capture().then(async function (details) {
+      console.log("details============================");
+      let amt = await props.getAmt();
+      if (details.status === "COMPLETED") {
+        try {
+          let res = await AddPayPalpay(data, user_id, amt);
+          if (res.status === 1 || "1") {
+            toast.success("Payment Successful.", {
+              position: toast.POSITION.TOP_RIGHT,
+              autoClose: 5000,
+            });
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      // getAccessToken()
+      //   .then((accessToken) => {
+      //     console.log("Access Token:", accessToken);
+      //     // Use this access token in your API requests
+      //   })
+      //   .catch((error) => {
+      //     console.error("Error getting access token:", error.message);
+      //   });
+
+      // Send the payment details to your server for validation and database update
+      // fetch('/api/paypal/capture', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({
+      //     orderID: data.orderID,
+      //     payerID: data.payerID,
+      //     paymentID: details.id,
+      //   }),
+      // })
+      // .then((response) => response.json())
+      // .then((data) => {
+      //   // Handle the response from your server
+      //   console.log('Server response:', data);
+      // })
+      // .catch((error) => {
+      //   console.error('Error:', error);
+      // });
+    });
+  };
+
+  return (
+    <>
+      {/* payment-------------------------- */}
+      <PayPalScriptProvider
+        options={{
+          "client-id":
+            "AbYsUSCVrRqDtp4zlox3JSNxRHmmc-SRYVqaFc7GFQzfveGacibhiJCignxz418kGQgbjwwg_AAVvX6V",
+        }}
+      >
+        <PayPalButtons createOrder={createOrder} onApprove={onApprove} />
+      </PayPalScriptProvider>
+      <br />
+
+      {/* token get ---------------------- */}
+      {/* <button
+        onClick={() => {
+          getAccessToken()
+            .then((accessToken) => {
+              console.log("Access Token:", accessToken);
+              // Use this access token in your API requests
+            })
+            .catch((error) => {
+              console.error("Error getting access token:", error.message);
+            });
+        }}
+      >
+        get token
+      </button> */}
+
+      {/* <button
+        onClick={() => {
+          getTransactionDetails("0WN30938H97935617")
+            .then((transactionDetails) => {
+              console.log("Transaction Details:", transactionDetails);
+              // Process and display the details as needed
+            })
+            .catch((error) => {
+              // Handle errors
+            });
+        }}
+      > 
+        get payment detaile
+      </button>*/}
+    </>
+  );
+};
+
+export default PayPalButton;
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fd447d18beacdf13fb98c812ed44eec.js"
+  "/precache-manifest.7c583020b109cde9cf9d9627fcdea6e7.js"
 );
 
 workbox.clientsClaim();
