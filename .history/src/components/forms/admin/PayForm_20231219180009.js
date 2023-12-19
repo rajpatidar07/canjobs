e80431856a459a5b91b8630aa164ebfcb7088f80
Aashi vyas <141002722+Aashi.we2code@users.noreply.github.com>
2023-12-19 18:15:28 +0530

    minor changes.

--- .history/src/components/forms/admin/PayForm_20231219180009.js
+++ .history/src/components/forms/admin/PayForm_20231219180009.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useValidation from "../../common/useValidation";
 import { toast } from "react-toastify";
 import { useLocation, useNavigate } from "react-router-dom";
