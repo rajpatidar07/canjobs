942871d87dd23c08dae571bd5dea2e1ad7f2e766
Aashi vyas <141002722+Aashi.we2code@users.noreply.github.com>
2023-12-29 11:26:25 +0530

    Created build

---
+++ build/precache-manifest.c938a86f3fac935d58fe5a1b4c2cb67d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f142c1335c56892bbd43",
+    "url": "/static/css/main.c8be17c6.chunk.css"
+  },
+  {
+    "revision": "f142c1335c56892bbd43",
+    "url": "/static/js/main.cdc5333d.chunk.js"
+  },
+  {
+    "revision": "2d2be85258a7f2628cb8",
+    "url": "/static/js/runtime~main.d1566087.js"
+  },
+  {
+    "revision": "75c02ddb890692aec898",
+    "url": "/static/css/2.dffc477b.chunk.css"
+  },
+  {
+    "revision": "75c02ddb890692aec898",
+    "url": "/static/js/2.4c1fe46d.chunk.js"
+  },
+  {
+    "revision": "74756de1ce4fdd488d68",
+    "url": "/static/js/3.f54a1483.chunk.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "ce38b86ea5173a6e60dc02a219a47094",
+    "url": "/static/media/verified.ce38b86e.png"
+  },
+  {
+    "revision": "2551aedf0df167535751e38c649e7caf",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8b1157930e26609e4a5ddceecdbc707.js"
+  "/precache-manifest.c938a86f3fac935d58fe5a1b4c2cb67d.js"
 );
 
 workbox.clientsClaim();
