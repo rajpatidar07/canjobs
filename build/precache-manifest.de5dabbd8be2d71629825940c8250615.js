7452d67af17a7010d8e75b70e4eaf3cb8a89c4f7
Aashi vyas <aashi.we2code@gmail.com>
2024-02-28 10:18:57 +0530

    Latest build.

---
+++ build/precache-manifest.de5dabbd8be2d71629825940c8250615.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "130edbf65205d29e5e46",
+    "url": "/static/css/main.5a0eb662.chunk.css"
+  },
+  {
+    "revision": "130edbf65205d29e5e46",
+    "url": "/static/js/main.b16d3aa8.chunk.js"
+  },
+  {
+    "revision": "3c44208740033762ff82",
+    "url": "/static/js/runtime~main.0014bda8.js"
+  },
+  {
+    "revision": "fd8650a56ec495bfa5e8",
+    "url": "/static/css/2.dffc477b.chunk.css"
+  },
+  {
+    "revision": "fd8650a56ec495bfa5e8",
+    "url": "/static/js/2.8f081fcf.chunk.js"
+  },
+  {
+    "revision": "82a3bb1b74d77836466b",
+    "url": "/static/js/3.fd73506e.chunk.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "ce38b86ea5173a6e60dc02a219a47094",
+    "url": "/static/media/verified.ce38b86e.png"
+  },
+  {
+    "revision": "4458c0d49795ab77f804fa594986f375",
+    "url": "/index.html"
+  }
+];
--- src/store/constants.js
+++
@@ -1,1 +0,0 @@
-export const LOGIN_ACTION_KEY = "LOGIN_ACTION_KEY";
