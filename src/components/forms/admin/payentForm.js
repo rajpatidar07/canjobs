bfd7ccb1c5c6e0052fcadf1c67264e6d7b31e0c2
Aashiwe2code <123159861+Aashiwe2code@users.noreply.github.com>
2024-11-04 18:00:10 +0530

    Latest code at 04/11/24 on 6.00pm.

--- src/components/forms/admin/payentForm.js
+++ src/components/forms/admin/payentForm.js
@@ -46,7 +46,7 @@ export default function PayentForm({ data, user_id, user_type }) {
 
   return (
     <>
-      
+
       <div className="bg-white rounded h-100 px-10 overflow-y-hidden">
         {/* <h5 className="text-center pt-2 mb-7">Follow Ups</h5> */}
         <div className="row">
@@ -54,71 +54,73 @@ export default function PayentForm({ data, user_id, user_type }) {
             // loading ? (
             //   <Loader load={"yes"} />
             // ) :
-            <div className="activity_container col-md-8 border-right">
-              {paymentList.length === 0 ? (
-                <div className="single_note mb-5">
-                  <div className="d-flex justify-content-center">
-                    <p className="text-italic font-size-3 m-0">No Data Found</p>
-                  </div>
-                </div>
-              ) : (
-                (paymentList || []).map((res, index) => (
-                  <div className="single_note mb-5" key={index}>
-                    <div className="d-flex justify-content-between">
-                      <p className="text-italic font-size-3 m-0">
-                        Payment on:{" "}
-                        <ConvertTime _date={res.created_at} format={"Do MM YYYY, h:mm:ss a"}/>
-                        {/* {moment(res.created_at).format("Do MM YYYY, h:mm:ss a")} */}
-                      </p>
+            <div className="activity_container col-md-8 border-right ">
+              <div className="single_note mb-5">
+                {paymentList.length === 0 ? (
+                  <div className="">
+                    <div className="d-flex justify-content-center">
+                      <p className="text-italic font-size-3 m-0">No Data Found</p>
                     </div>
-                    <div className="card rounded-3 py-2 px-5">
-                      <p className="fw-bold m-0 row">
-                        <span className="col-md-10 col-sm-12 ">
-                          {res.payment_mode ? (
-                            <>
-                              <span className="text-break text-capitalize">
-                                <b>Payment mode</b>: {res.payment_mode}
+                  </div>
+                ) : (
+                  (paymentList || []).map((res, index) => (
+                    <div className="" key={index}>
+                      <div className="d-flex justify-content-between">
+                        <p className="text-italic font-size-3 m-0">
+                          Payment on:{" "}
+                          <ConvertTime _date={res.created_at} format={"Do MM YYYY, h:mm:ss a"} />
+                          {/* {moment(res.created_at).format("Do MM YYYY, h:mm:ss a")} */}
+                        </p>
+                      </div>
+                      <div className="card rounded-3 py-2 px-5">
+                        <p className="fw-bold m-0 row">
+                          <span className="col-md-10 col-sm-12 ">
+                            {res.payment_mode ? (
+                              <>
+                                <span className="text-break text-capitalize">
+                                  <b>Payment mode</b>: {res.payment_mode}
+                                </span>
+                                <br />
+                              </>
+                            ) : null}
+                            {res.payment_id ? (
+                              <>
+                                <span className="text-break">
+                                  <b>Payment Id</b>: {res.payment_id}
+                                </span>
+                                <br />
+                              </>
+                            ) : null}
+                            {res.order_id ? (
+                              <>
+                                <span className="text-break">
+                                  <b>order Id</b>: {res.order_id}
+                                </span>
+                                <br />
+                              </>
+                            ) : null}
+                            {res.amount ? (
+                              <span>
+                                <b>Total Amount</b>: {res.amount}/-
                               </span>
-                              <br />
-                            </>
-                          ) : null}
-                          {res.payment_id ? (
-                            <>
-                              <span className="text-break">
-                                <b>Payment Id</b>: {res.payment_id}
-                              </span>
-                              <br />
-                            </>
-                          ) : null}
-                          {res.order_id ? (
-                            <>
-                              <span className="text-break">
-                                <b>order Id</b>: {res.order_id}
-                              </span>
-                              <br />
-                            </>
-                          ) : null}
-                          {res.amount ? (
-                            <span>
-                              <b>Total Amount</b>: {res.amount}/-
-                            </span>
-                          ) : null}
-                        </span>
-                        <span className="col-md-2 col-sm-12">
-                          <span
-                            className={`${res.status === "success"
-                              ? " bg-primary-opacity-8 text-white text-center w-100  rounded-pill"
-                              : " bg-warning text-white text-center w-100  rounded-pill"
-                              }`}
-                          >
-                            {res.status}
+                            ) : null}
                           </span>
-                        </span>
-                      </p>
+                          <span className="col-md-2 col-sm-12">
+                            <span
+                              className={`${res.status === "success"
+                                ? " bg-primary-opacity-8 text-white text-center w-100  rounded-pill"
+                                : " bg-warning text-white text-center w-100  rounded-pill"
+                                }`}
+                            >
+                              {res.status}
+                            </span>
+                          </span>
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                ))
-              )}
+                  ))
+                )}
+              </div>
             </div>
           }
           {(user_type === "employee" && user === "agent") ? null :
