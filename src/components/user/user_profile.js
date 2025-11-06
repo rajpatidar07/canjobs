dc1609316db543aa082497378be26f2e10616ceb
Aashiwe2code <123159861+Aashiwe2code@users.noreply.github.com>
2025-11-06 17:30:12 +0530

    Added phone number to the ringcentral embed code.

--- src/components/user/user_profile.js
+++ src/components/user/user_profile.js
@@ -1263,7 +1263,7 @@ const NewUserProfile = (props) => {
                                   <div>
                                     <Link
                                       className="font-size-3 text-break btn btn-outline-secondary btn-rounded px-4"
-                                      // to={`tel:${PersonalDetail.contact_no}`}
+                                      to={`tel:${PersonalDetail.contact_no}`}
                                       onClick={() => makeCall(PersonalDetail.contact_no, PersonalDetail.name, PersonalDetail.profile_photo
                                         ? PersonalDetail.profile_photo
                                         : `https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png`)}
